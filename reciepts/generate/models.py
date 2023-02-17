8692ee6a62592e9ed6306617f7ef89b549c3585e
Michael David <davidlearn27@gmail.com>
2023-02-18 00:33:16 +0100

    tested projects in models and django admin

--- reciepts/generate/models.py
+++ reciepts/generate/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import inflect
 # Create your models here.
 
 
@@ -14,11 +14,18 @@ class Reciepts(models.Model):
     name_student = models.CharField(max_length=100)
     name_Payer = models.CharField(max_length=100)
     description = models.CharField(max_length=120)
+    amount_in_words = models.CharField(max_length=120, null=True, blank=True)
     date_created = models.DateField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        self.number = self.pk + last_payment_id
+        words = inflect.engine()
+        words = words.number_to_words(self.amount)
+        self.amount_in_words = words
         return super().save(*args, **kwargs)
+
+    @property
+    def reciept(self):
+        return self.pk + last_payment_id
 
     def __str__(self):
         return f'{self.name_student} {self.amount}'
--- reciepts/generate/views.py
+++ reciepts/generate/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Reciepts
 
 # Create your views here.
+
+
+def home(request):
+
+    return render(request, 'reciepts/home.html')
+
+
+def reciepts(request):
+
+
+
+    context = {}
+    if request.method == 'POST':
+        data = Reciepts()
+        data.amount = request.data['amount']
+        data.name_student = request.data['name_student']
+        data.name_Payer = request.data['name_Payer']
+        data.description = request.data['description']
+
+
+
+    return render(request, 'reciepts/home.html')
