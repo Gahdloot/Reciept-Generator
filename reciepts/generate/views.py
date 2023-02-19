e87a7bb726a789a24276dc5003009d5439785c53
Michael David <davidlearn27@gmail.com>
2023-02-19 13:23:16 +0100

    Move post method to home route

--- reciepts/generate/views.py
+++ reciepts/generate/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from .models import Reciepts
+from django.urls import reverse
 
 # Create your views here.
 
 
 def home(request):
-
-    return render(request, 'reciepts/home.html')
-
-
-def reciepts(request):
-
-
-
-    context = {}
-    if request.method == 'POST':
-        data = Reciepts()
-        data.amount = request.data['amount']
-        data.name_student = request.data['name_student']
-        data.name_Payer = request.data['name_Payer']
-        data.description = request.data['description']
+    if request.method == 'GET':
+        return render(request, 'reciepts/home.html')
+    else:
+        if request.method == 'POST':
+            data = Reciepts()
+            data.amount = request.data['amount']
+            data.name_student = request.data['name_student']
+            data.name_Payer = request.data['name_Payer']
+            data.description = request.data['description']
+            data.save()
+            return HttpResponseRedirect(reverse('reciepts:reciept', args=(data.reciept_number,)))
 
 
-
-    return render(request, 'reciepts/home.html')
+def reciepts(request, reciept):
+    data = Reciepts.objects.get(reciept_number=reciept)
+    context = {'Reciepts': data}
+    return render(request, 'reciepts/home.html', context)
